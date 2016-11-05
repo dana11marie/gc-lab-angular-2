455cd7af2e48d6ee667486001a649b7a558cf61c
Dana Dietrich <dana.m.dietrich11@gmail.com>
2016-11-05 14:52:08 -0400

    commit2

--- ctrl1.js
+++ ctrl1.js
@@ -1,6 +1,14 @@
 var app = angular.module('myModule');
 
 app.controller('ctrlOne', function($scope){
-
-  
+  $scope.palindrome = function(word){ //parameter can be named anything we want, but it makes sense to make it the same as the ng-model.
+    console.log(word);
+    var originalWord = word;
+    var reverseWord = word.split('').reverse().join('');
+    if (originalWord === reverseWord) {
+      $scope.result = "Yes!";
+    } else {
+      $scope.result = "Nope.";
+    };
+  };
 });
